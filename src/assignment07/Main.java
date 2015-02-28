c5d7fe3d93649bab2ea39470f68e3938036377d7
Reardonp <REardonp@gmail.com>
2015-02-28 12:15:59 -0500

    Created Main
    Created Main for Assignment07

---
+++ src/assignment07/Main.java
@@ -0,0 +1,11 @@
+package assignment07;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MultiplesOfThreesAndFives a = new MultiplesOfThreesAndFives (1000);
+		System.out.println("The results = " + a);
+	}
+
+}
